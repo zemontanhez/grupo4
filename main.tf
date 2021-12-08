bc9dcd63641880c01c2a9384544d236138fe0fbc
zemontanhez <80980227+zemontanhez@users.noreply.github.com>
2021-12-08 15:59:13 -0300

    Update main.tf
    limpeza de comentario

--- main.tf
+++ main.tf
@@ -1,74 +1,3 @@
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-# resource "aws_instance" "web" {
-#   ami           = "ami-09e67e426f25ce0d7"
-#   instance_type = "t2.micro"
-#   subnet_id              = "subnet-eddcdzz4"
-#   # availability_zones = ["us-east-1"] # configurando zona para criar maquinas
-#   tags = {
-#     Name = "Minha Maquina Simples EC2"
-#   }
-#   volume_id = aws_ebs_volume.itau_volume_encrypted.id
-# }
-
-
-# resource "aws_ebs_volume" "itau_volume_encrypted" {
-#   size      = 8
-#   encrypted = true
-#   tags      = {
-#     Name = "Volume itaú"
-#   }
-# }
-
-
-
-##### Caso Itaú #####
-# aws configure # maquina pessoal
-# nas do itaú colocar as variáveis de ambiente no bashrc
-# https://docs.aws.amazon.com/sdkref/latest/guide/environment-variables.html
-# configurar via environment variable
-# export AWS_ACCESS_KEY_ID=""
-# export AWS_SECRET_ACCESS_KEY=""
-# export AWS_DEFAULT_REGION=""
-
-# provider "aws" {
-#   region = "us-east-1"
-# }
-
-# resource "aws_instance" "web" {
-#   ami = "ami-09e67e426f25ce0d7"
-#   instance_type = "t3.micro"
-#   subnet_id = "subnet-05880ea9006199004"
-  
-#   tags = {
-#     Name = "MinhaPrimeiraMaquina-Carol  "
-#   }
-# }
-
-# resource "aws_ebs_volume" "itau_volume_encrypted" {
-#   size      = 8
-#   encrypted = true
-#   availability_zone = "us-east-1a"
-#   tags      = {
-#     Name = "Volume itaú"
-#   }
-# }
-
-# resource "aws_volume_attachment" "ebs_att" {
-#   device_name = "/dev/sdh"
-#   volume_id   = aws_ebs_volume.itau_volume_encrypted.id
-#   instance_id = aws_instance.web.id
-# }
-
-# Gamaacademythree-sbx - # passago a chave pelo pessoal de segurança itaú
-# export AWS_ACCESS_KEY_ID="3232323232"
-# export AWS_SECRET_ACCESS_KEY="34433444sssdd3+ssa/dd434343"
-
-# //////
-
-# ///////// do fernando zerati //////
 provider "aws" {
   region = "sa-east-1"
 }
@@ -156,10 +85,6 @@ resource "aws_instance" "web2" {
     volume_size = var.tamanho
   }
 
-#  count = length(keys(var.maquinas))
-#  filename = "${keys(var.maquinas)[count.index]}.txt"
-#  content = var.maquinas[keys(var.maquinas)[count.index]]
-
   tags = {
     Name = "turma3-ec2-ze-tf-${var.Name}"
     #Tag2 = "Tag teste - ${var.maquinas[keys(var.maquinas)[count.index]]}"
@@ -177,14 +102,3 @@ output "maquina_aws" {
 #ami-0e66f5495b4efdd0f
 #sg-0c493a481025a9028 (secgroup-turma3-ze)
 #keypair-turma3-ze-dev
-
-
-#resource "aws_key_pair" "chave_key" {
-#  key_name   = "chave_key"
-#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABUAHUAHuaHaUhAUhAUAiqHA541BsJFngWPqlx27QdAZEWdMLvJv5Wguvatb6LIDo1V3rJ4mUtRRs0o2q3LwYiA5CIkiHFXyNVhXTF1WNAbRossUMsu/BzmgEKyIPPgPHeM0PyRi6FuW1TTZYdnO/GCzJ0UMvZFKnr2g6rELWgdc9Clxz8peNJ+iPJx/sJb+DxTuHDJc1U9eOYS7vlwzsHHApD9O+DbWnpwRpSEuX3vjm5pEEAPqrcBD3HK8kH2qMVRZNxg/fSzSrzjCwFV3ShNbKSTD6HYBV2xCY18mRFjyW94BPSBDGel7/kqTmXY4jtbAoyycWRZJFYhCdzNfItT69nHmsT3i09e0J9jNI6CaameQg/cwIOt8fl+lxUIAufHqFDJPGMJcNFoVR7t7yWPXN3qev2OlGnQONDVlNOmIJDrO+r2QeoVcKaxKye7G3HD3u4HuqGYfL9MtCo6pOZ8IZsCCj2KpS4KQCc="
-#}
-
-
-# /////
-
-# aws_instance.web.public_id
